da2aa8d71cbfc3a224632546bca7822aa7971bb1
Toby Fee <TobyFee@gmail.com>
2019-07-22 11:54:21 -0700

    short burst of logging

---
+++ logfiller.js
@@ -0,0 +1,13 @@
+const readline = require('readline');
+const fs = require('fs');
+
+
+const readInterface = readline.createInterface({
+    input: fs.createReadStream('short_log'),
+    output: process.stdout,
+    console: false
+});
+
+readInterface.on('line', function(line) {
+    console.log(line);
+});
